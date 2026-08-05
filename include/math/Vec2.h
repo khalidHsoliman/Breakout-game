4dba157824bb7b818f7735c0385c1b246d85e51f
khalidHsoliman <khalidhsoliman@gmail.com>
2026-08-05 19:56:50 +0300

    put Vec2 return types on the same line as the function name

--- include/math/Vec2.h
+++ include/math/Vec2.h
@@ -10,39 +10,33 @@ namespace math
     };
 
     // Unary operators
-    inline Vec2
-    operator-(Vec2 a)
+    inline Vec2 operator-(Vec2 a)
     {
         return Vec2{-a.x, -a.y};
     }
 
     // Binary operators
-    inline Vec2
-    operator+(Vec2 a, Vec2 b)
+    inline Vec2 operator+(Vec2 a, Vec2 b)
     {
         return Vec2{a.x + b.x, a.y + b.y};
     }
 
-    inline Vec2
-    operator-(Vec2 a, Vec2 b)
+    inline Vec2 operator-(Vec2 a, Vec2 b)
     {
         return Vec2{a.x - b.x, a.y - b.y};
     }
 
-    inline Vec2
-    operator*(Vec2 a, float b)
+    inline Vec2 operator*(Vec2 a, float b)
     {
         return Vec2{a.x * b, a.y * b};
     }
 
-    inline Vec2
-    operator*(float a, Vec2 b)
+    inline Vec2 operator*(float a, Vec2 b)
     {
         return Vec2{a * b.x, a * b.y};
     }
 
-    inline Vec2
-    operator/(Vec2 a, float b)
+    inline Vec2 operator/(Vec2 a, float b)
     {
         if (b == 0.0f)
         {
@@ -52,58 +46,50 @@ namespace math
     }
 
     // Assignment operators
-    inline Vec2&
-    operator+=(Vec2& a, Vec2 b)
+    inline Vec2& operator+=(Vec2& a, Vec2 b)
     {
         a.x += b.x;
         a.y += b.y;
         return a;
     }
 
-    inline Vec2&
-    operator-=(Vec2& a, Vec2 b)
+    inline Vec2& operator-=(Vec2& a, Vec2 b)
     {
         a.x -= b.x;
         a.y -= b.y;
         return a;
     }
 
-    inline Vec2&
-    operator*=(Vec2& a, float b)
+    inline Vec2& operator*=(Vec2& a, float b)
     {
         a.x *= b;
         a.y *= b;
         return a;
     }
 
-    inline Vec2&
-    operator/=(Vec2& a, float b)
+    inline Vec2& operator/=(Vec2& a, float b)
     {
         a = a / b;
         return a;
     }
 
     // Utility functions
-    inline float
-    length_squared(Vec2 a)
+    inline float length_squared(Vec2 a)
     {
         return a.x * a.x + a.y * a.y;
     }
 
-    inline float
-    length(Vec2 a)
+    inline float length(Vec2 a)
     {
         return std::sqrt(length_squared(a));
     }
 
-    inline float
-    distance(Vec2 a, Vec2 b)
+    inline float distance(Vec2 a, Vec2 b)
     {
         return length(a - b);
     }
 
-    inline Vec2
-    normalize(Vec2 a)
+    inline Vec2 normalize(Vec2 a)
     {
         const float len = length(a);
         if (len == 0.0f)
@@ -113,8 +99,7 @@ namespace math
         return a / len;
     }
 
-    inline float
-    dot(Vec2 a, Vec2 b)
+    inline float dot(Vec2 a, Vec2 b)
     {
         return a.x * b.x + a.y * b.y;
     }
